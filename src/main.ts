05091c3370057efa8fa1b26a11e20f1017b3bd44
Mikhail Khlus <khlus.2003@mail.ru>
2025-06-27 23:06:57 +0300

    feat: add custom select

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
 import "./scss/style.scss";
+
+const wrapper = document.querySelector(".custom-select__wrapper");
+const dropdown = wrapper.querySelector(".custom-select__dropdown");
+const selected = wrapper.querySelector(".custom-select__selected span");
+const options = wrapper.querySelectorAll(".custom-select__option");
+
+wrapper.addEventListener("click", () => {
+  dropdown.classList.toggle("custom-select__dropdown-visible");
+  wrapper.classList.toggle("custom-select__wrapper-active");
+});
+
+options.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    e.stopPropagation();
+    selected.textContent = option.textContent;
+
+    options.forEach((o) => o.classList.remove("custom-select__option-active"));
+    option.classList.add("custom-select__option-active");
+
+    dropdown.classList.remove("custom-select__dropdown-visible");
+    wrapper.classList.remove("custom-select__wrapper-active");
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (!wrapper.contains(e.target)) {
+    dropdown.classList.remove("custom-select__dropdown-visible");
+    wrapper.classList.remove("custom-select__wrapper-active");
+  }
+});
